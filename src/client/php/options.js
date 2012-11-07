bda7bc1a86336f3ae3bbe7a02511a2cd94fde24f
mattPress <matt@matthewpress.com>
2012-11-07 00:24:10 +0000

    php options. Moved inline code to independent file to solve security issues.

---
+++ src/client/php/options.js
@@ -0,0 +1,58 @@
+ console.log( "hello, goodbye" );
+ 
+ function clearStatus() {
+                
+                document.getElementById( "status" ).innerHTML = '';
+            };
+        
+            function selectionChanged() {
+                
+                var selected_option = this.options[ this.selectedIndex ];
+                var value = selected_option.value;
+                if ( value == "www.php.net/" ) {
+                    
+                    delete localStorage[ 'mirror' ];
+                } else {
+                    
+                    localStorage[ 'mirror' ] = value;
+                }
+                
+                document.getElementById( "status" ).innerHTML = '<font color=\"green\">Changes saved.</font>';
+                setTimeout( clearStatus, 1000 );
+            };
+        
+            var total = 1;
+            var req = new XMLHttpRequest();
+            req.open( "GET", "http://www.php.net/mirrors.php" );
+            req.onreadystatechange=function(){
+                
+                if ( req.readyState == 4 ){
+                    
+                    var status=req.status;
+                    if ( ( status == 200 ) || ( status == 301 ) || ( status == 302 ) ) {
+                        
+                        var text = req.responseText;
+                        var matches = text.match( new RegExp( "<td><a href=\"http://([a-z]|[A-Z]|[0-9])+\.php\.net/\">([a-z]|[A-Z]|[0-9])+\.php\.net</a></td>", "g" ) );
+                        for ( var i = 0; i < matches.length; ++i ) {
+                            
+                            var match = matches[ i ];
+                            var hrefstartiddx = match.indexOf( "http://" ) + 7;
+                            var hrefendidx = match.indexOf( "/", hrefstartiddx );
+                            var value = match.substring( hrefstartiddx, hrefendidx );
+                            if ( value != "www.php.net" ) {
+                                
+                                document.extension_configuration.mirror_sites.options[ total ] = new Option( value, value );
+                                if ( localStorage[ 'mirror' ] && ( value == localStorage[ 'mirror' ] ) ) {
+                                    
+                                    document.extension_configuration.mirror_sites.selectedIndex = total;
+                                }
+                                total++;
+                            }
+                        }
+                        document.extension_configuration.mirror_sites.onchange = selectionChanged;
+                        document.getElementById( "status" ).innerHTML = '';
+                        document.getElementById( "content" ).style.display = "block";
+                    }
+                }
+            };
+            req.send( null );
